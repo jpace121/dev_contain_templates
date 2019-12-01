c13c57e36d1294810b8b324c21d03ef4f2ecc6a6
James Pace <jpace121@gmail.com>
2019-12-01 18:40:35 -0500

    Make tmux in tmux work better. Set special color in container window.

--- templates/entrypoints/tmux.bash
+++ templates/entrypoints/tmux.bash
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+export TERM=xterm-256color
 
 tmux new -s container -d
+
+# Set additional prefix so 'tmux in tmux' works.
+tmux set -g prefix2 'C-b'
+tmux bind 'C-b' send-prefix -2
+# Set unique to container background color.
+tmux set -g status-bg blue
+ 
 if [ $# -gt 0 ]
 then
     tmux send -t container "$@" ENTER

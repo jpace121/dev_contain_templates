adaf3a282ae337693ea13197e6cdd716b54d4b26
James Pace <jpace121@gmail.com>
2019-12-02 23:12:48 -0500

    Name our container.

--- templates/entrypoints/tmux.bash
+++ templates/entrypoints/tmux.bash
@@ -2,9 +2,9 @@
 # Make colors work.
 export TERM=xterm-256color
 # Make sure used environment variables are set.
-export PROJECT_NAME="${PROJECT_NAME:=dev}"
+export CONTAINER_NAME="${CONTAINER_NAME:=dev}"
 # Make new session.
-tmux new -s $PROJECT_NAME -d
+tmux new -s $CONTAINER_NAME -d
 # Set additional prefix so 'tmux in tmux' works.
 tmux set -g prefix2 'C-b'
 tmux bind 'C-b' send-prefix -2
@@ -13,4 +13,4 @@ tmux set-option -g status-right ""
 # Set unique to container background color.
 tmux set -g status-bg blue
 # Attach.
-exec tmux attach -t $PROJECT_NAME
+exec tmux attach -t $CONTAINER_NAME

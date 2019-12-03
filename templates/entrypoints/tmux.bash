e13233332c2067ffb5d95f1bc2264c6168113c10
James Pace <jpace121@gmail.com>
2019-12-02 22:36:07 -0500

    Set project name in run script. Pass as environment variable.

--- templates/entrypoints/tmux.bash
+++ templates/entrypoints/tmux.bash
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 # Make colors work.
 export TERM=xterm-256color
+# Make sure used environment variables are set.
+export PROJECT_NAME="${PROJECT_NAME:=dev}"
 # Make new session.
-tmux new -s container -d
+tmux new -s $PROJECT_NAME -d
 # Set additional prefix so 'tmux in tmux' works.
 tmux set -g prefix2 'C-b'
 tmux bind 'C-b' send-prefix -2
+# Remove right status line.
+tmux set-option -g status-right ""
 # Set unique to container background color.
 tmux set -g status-bg blue
-# Send commands, if any to tmux.
-if [ $# -gt 0 ]
-then
-    tmux send -t container "$@" ENTER
-fi
 # Attach.
-exec tmux attach -t container
+exec tmux attach -t $PROJECT_NAME

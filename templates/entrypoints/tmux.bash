14fc6c4de3c9dda628db2a8025a1d09d969281cd
James Pace <jpace121@gmail.com>
2019-12-04 03:19:13 +0000

    Getting somewhere with leaving the container running.

--- templates/entrypoints/tmux.bash
+++ templates/entrypoints/tmux.bash
@@ -3,6 +3,8 @@
 export TERM=xterm-256color
 # Make sure used environment variables are set.
 export CONTAINER_NAME="${CONTAINER_NAME:=dev}"
+# Start tmux
+tmux start-server
 # Make new session.
 tmux new -s $CONTAINER_NAME -d
 # Set additional prefix so 'tmux in tmux' works.
@@ -13,4 +15,5 @@ tmux set-option -g status-right ""
 # Set unique to container background color.
 tmux set -g status-bg blue
 # Attach.
-exec tmux attach -t $CONTAINER_NAME
+#exec tmux attach -t $CONTAINER_NAME
+exec sleep +Inf

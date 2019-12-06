931e35236561fe0806598f19111c7d3d316ad6ae
James Pace <jpace121@gmail.com>
2019-12-05 22:06:32 -0500

    Don't actually need to launch tmux.

--- templates/entrypoints/tmux.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Make sure used environment variable is set.
-export DEV_CONTAIN_CONTAINER_NAME="${DEV_CONTAIN_CONTAINER_NAME:=dev}"
-# Make new session.
-tmux new -s $DEV_CONTAIN_CONTAINER_NAME -d
-exec sleep +Inf

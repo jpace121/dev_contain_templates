e4ca589fb7cca16fb2c706d25863333050ef164a
James Pace <jpace121@gmail.com>
2020-01-25 22:08:06 -0500

    j7s rasbian-ish build.

---
+++ templates/scripts/build-ros-from-source.bash
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+# Build ROS 2 Eloquent from scratch.
+sudo apt update -y && sudo apt install -y curl gnupg2 lsb-release
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
+
+sudo apt update && sudo apt install -y \
+  build-essential \
+  cmake \
+  git \
+  python3-colcon-common-extensions \
+  python3-pip \
+  python-rosdep \
+  python3-vcstool \
+  wget
+# install some pip packages needed for testing
+python3 -m pip install -U \
+  argcomplete \
+  flake8 \
+  flake8-blind-except \
+  flake8-builtins \
+  flake8-class-newline \
+  flake8-comprehensions \
+  flake8-deprecated \
+  flake8-docstrings \
+  flake8-import-order \
+  flake8-quotes \
+  pytest-repeat \
+  pytest-rerunfailures \
+  pytest \
+  pytest-cov \
+  pytest-runner \
+  setuptools
+# install Fast-RTPS dependencies
+sudo apt install --no-install-recommends -y \
+  libasio-dev \
+  libtinyxml2-dev
+
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws
+vcs import src < /eloquent-release.rosinstall
+
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro eloquent -y --skip-keys "console_bridge fastcdr fastrtps libopensplice67 libopensplice69 rti-connext-dds-5.3.1 urdfdom_headers"
+
+cd ~/ros2_ws/
+colcon build --symlink-install
